5f82dda64bace0f302fc838279c558753fb36c7b
itmeVN <Buituanv@gmail.com>
2018-09-07 12:29:46 +0700

    Update server.js

--- QuestionVote/Server/server.js
+++ QuestionVote/Server/server.js
@@ -81,7 +81,7 @@ app.put('/answer',(req,res) => {
    
 });
 
-app.get('/questionInfor',(req,res) => {
+app.get('/questionInfor/:questionId',(req,res) => {
     const questionId = req.body.id;
     console.log(questionId);
     QuestionModel.findById(questionId,(err, questionFound) => {
@@ -93,101 +93,6 @@ app.get('/questionInfor',(req,res) => {
     })
 });
 
-// app.get('/questionInfor',(req,res) =>{
-//     const questionID = req.body;
-//     QuestionModel.fin
-// })
-// app.get('/',(req,res) => {
-//     res.send("Hello world");
-// });
-
-
-// app.post('/ask',(req,res) =>{
-//     console.log(req.body.question);
-//     fs.readFile('./questions.txt', (err,fileData) =>{
-//         if(err) console.log(err)
-//         else
-//             try{
-               
-//                 let questions = [];
-//                 if(fileData > 0 && JSON.parse(fileData).length) {
-//                     questions = JSON.parse(fileData);
-//                     const newQuestion  = {
-//                         id: questions.length + 1,
-//                         question: req.body.question,
-//                         yes:0,
-//                         no:0
-//                     }
-//                }
-               
-//                 questions.push(newQuestion);
-//                 fs.writeFile('./questions.txt',JSON.stringify(questions), (err) =>{
-//                     if(err) console.log(err)
-//                     else res.redirect('http://localhost:8080');
-//                 });
-//             }
-//             catch (error){
-//                 console.log(error);
-//             }
-      
-//     });
-// });
-
-// app.get('/question',(req,res) =>{
-//     fs.readFile('./questions.txt', (err,fileData) =>{
-//         if(err) console.log(err)
-//         else{
-//             try{
-//                 let questions = JSON.parse(fileData);
-//                 let RadomNum = Math.floor(Math.random()* questions.length); 
-//                 let RandomQuestion = questions[RadomNum];
-//                 res.send(RandomQuestion)
-//             }
-//             catch(err){
-//                 console.log("Error: " + err);
-//             }
-//         }
-//     })
-// });
-
-// app.put('/answer',(req,res) =>{
-//     const {answer,questionID} =req.body;
-//     fs.readFile('./questions.txt', (err,fileData) =>{
-//         if(err) console.log(err)
-//         else{
-//             try{
-//                 let questions = [];
-//                 if(questions.length >0 && JSON.parse(fileData).length){
-//                     questions = JSON.parse(fileData);
-//                 }
-//                 // for(let i=0;i< questions.length;i++){
-//                 //     // if(questions[i].id = questionID){
-//                 //     //     if((answer) == "yes")
-//                 //     //         questions[i].yes +=1;
-//                 //     //     else
-//                 //     //         questions[i].no +=1;
-//                 //     // }
-                   
-//                 // }
-//                 if(questions[questionID-1]){
-//                     questions[questionID-1][answer] +=1;
-//                 }
-//                 fs.writeFile('./questions.txt',JSON.stringtyfy(questions),(err)=>{
-//                     if(err) console.log(err);
-//                     else
-//                         res.send({message: 'Success',question : questions[questionID -1]});
-//                 })
-//             }
-//             catch(err){
-//                 console.log("Error: " + err);
-//             }
-//         }
-//     })
-// });
-
-// app.get('/question/:questionID',(req,res)=>{
-//     console.log(req.params.questionID);
-// })
 app.listen(6969, (err) =>{
     if(err) console.log(err);
     else console.log("Server is running at port 6969");
